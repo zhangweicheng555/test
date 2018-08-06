1ab7727e0837657e0722cd557f366a2e2298ce9a
a-zhangweicheng <a-zhangweicheng@L-080320-1.mshome.net>
2018-08-06 20:47:20 +0800

    111

--- boot-security/src/main/java/com/boot/security/server/controller/UserController.java
+++ boot-security/src/main/java/com/boot/security/server/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.boot.security.server.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +32,23 @@ public class UserController {
 		return sysUserService.findTest(beginDate,endDate);
 	}
 
+	
+	public static void main(String[] args) throws ParseException {
+		String begin="2018/7/7 12:00";
+		String end="2018/7/7 13:10";
+		SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+		Date beginData=sdf.parse(begin);
+		Date endData=sdf.parse(end);
+		List<Date> listDates=new ArrayList<>();
+		for (int i = 0; i < 10000; i++) {
+			Date afterDate = new Date(beginData.getTime() + 5*60*1000);
+			if (afterDate.compareTo(endData) > 0) {
+				break;
+			}
+			listDates.add(afterDate);
+		}
+		for (Date date : listDates) {
+			System.out.println(sdf.format(date));
+		}
+	}
 }
