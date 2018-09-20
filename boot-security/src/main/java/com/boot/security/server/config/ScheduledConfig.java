f5995d8e7fdd858df51de73fa4e0d159569cfeb2
unknown <a-zhangweicheng@L-080320-1.kuandeng.com>
2018-09-20 20:40:23 +0800

    bug修复   动态模拟实现

--- boot-security/src/main/java/com/boot/security/server/config/ScheduledConfig.java
+++ boot-security/src/main/java/com/boot/security/server/config/ScheduledConfig.java
@@ -1,6 +1,8 @@
 package com.boot.security.server.config;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -20,19 +22,42 @@ public class ScheduledConfig {
 
 	/**
 	 * 0点三分执行
-	 * 确认入库的时间
+	 * 确认入库的时间  正式服
 	 */
-	@Transactional
-	@Scheduled(cron = "0 3 0 * * ?")
+	//@Transactional
+	//@Scheduled(cron = "0 3 0 * * ?")
 	public void execByThirtyMin() {
-		// 查询表中前四小时的数据
+		// 查询表中最大时间
 		String beforeDate = gridDataService.queryMaxDate();
 		// 将小于这个时间的插入到历史表中
 		gridDataService.insertBatch(beforeDate);
 		// LTE_GRID_USER_NUM删除 小于这个日期的数据
 		gridDataService.deleteBatch(beforeDate);
 	}
+	/**
+	 * 每五分钟执行一次
+	 * 测试服
+	 * @throws ParseException 
+	 */
+	@Transactional
+	@Scheduled(cron = "0 0/5 0 * * ?")
+	public void execByFiveMin() throws ParseException {
+		// 查询表中最大时间
+		String beforeDate = gridDataService.queryMaxDate();
+		//加5分钟
+		String nowDate=dealMaxDate(beforeDate);
+		// 更新表中的所有时间
+		//更新表中的所有人数  select ceil(dbms_random.value(1,42)) from dual;
+		//插入到新表中 
+	}
 
+	private  String dealMaxDate(String beforeDate) throws ParseException {
+		 SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		 Date date = sdf.parse(beforeDate);
+		 Date afterDate = new Date(date.getTime() + 300000);
+		return sdf.format(afterDate);
+	}
+	
 	/**
 	 * 下午2点到晚上8点 每5分钟执行一次
 	 */
