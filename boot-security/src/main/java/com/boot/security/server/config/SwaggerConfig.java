2c386f9a780eb2fa58574be78bf6f23ce40820a8
DESKTOP-LEDT8G5\dell <19348243@qq.com>
2018-09-23 11:22:26 +0800

    sawgger配置

--- boot-security/src/main/java/com/boot/security/server/config/SwaggerConfig.java
+++ boot-security/src/main/java/com/boot/security/server/config/SwaggerConfig.java
@@ -1,25 +1,37 @@
 package com.boot.security.server.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.Contact;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * swagger文档
  */
+@EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 
 	@Bean
 	public Docket docket() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("接口文档说明")
-				.apiInfo(new ApiInfoBuilder().title("接口文档")
-						.contact(new Contact("大数据可视化呈现技术接口", "", "19348243@qq.com")).version("1.0").build())
-				.select().paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("接口展示列表")
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.boot.security.server.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("2018国家会展中心可视化项目需求接口")
+								   .description("接口说明文档")
+				                   /*.termsOfServiceUrl(" API terms of service")*/
+				                   .version("20180923")
+				                   .build();
 	}
 }
