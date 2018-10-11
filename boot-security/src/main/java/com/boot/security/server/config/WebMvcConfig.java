a8f9f94d08f80c0b22a2bf5ff58ce468d3b9b711
unknown <a-zhangweicheng@L-080320-1.kuandeng.com>
2018-10-11 11:37:35 +0800

    新修改

--- boot-security/src/main/java/com/boot/security/server/config/WebMvcConfig.java
+++ boot-security/src/main/java/com/boot/security/server/config/WebMvcConfig.java
@@ -1,12 +1,28 @@
 package com.boot.security.server.config;
 
 import java.io.File;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.ResourceUtils;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -39,5 +55,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/statics/**")
 				.addResourceLocations(ResourceUtils.FILE_URL_PREFIX + filesPath + File.separator);
 	}
+
+	
 
 }
--- boot-security/src/test/java/com/boot/security/server/SecurityApplicationTest.java
+++ boot-security/src/test/java/com/boot/security/server/SecurityApplicationTest.java
@@ -1,17 +1,11 @@
 package com.boot.security.server;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.boot.security.server.model.GridMapper;
 import com.boot.security.server.service.GridMapperService;
-
-import oracle.net.aso.g;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
